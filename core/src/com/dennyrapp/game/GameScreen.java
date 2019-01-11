76738c6a99bfc63a1689a06759014975cfd37f41
Dcime <denny_R_96@web.de>
2019-01-11 02:07:19 +0100

    added tower

--- core/src/com/dennyrapp/game/GameScreen.java
+++ core/src/com/dennyrapp/game/GameScreen.java
@@ -14,13 +14,23 @@ public class GameScreen implements Screen {
 	
 	OrthographicCamera camera;
 	
-	Sprite sprite;
+	Sprite sprite,tower_sprite;
 	public GameScreen(FlappyGame game) {
 		this.game = game;
+		
 		sprite = new Sprite(game.harry);
 		int harry_width = (int)(game.harry.getWidth() * 0.125);
 		int harry_height = (int)(game.harry.getHeight() * 0.125);
 		sprite.setSize(harry_width, harry_height);
+		
+		tower_sprite = new Sprite(game.turm_gryffindor);
+		//int penis = Gdx.graphics.getWidth();
+		int tower_width = (int)(game.turm_gryffindor.getWidth() * 0.5);
+		int tower_height = (int)(game.turm_gryffindor.getHeight() * 0.5);
+		tower_sprite.setSize(tower_width, tower_height);
+		tower_sprite.setPosition(1150, -250);
+		
+		
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1280, 720);
 	}	
@@ -30,7 +40,7 @@ public class GameScreen implements Screen {
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(0, 0.5f, 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+		System.out.println(Gdx.graphics.getWidth());
 		if(Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
 			if(sprite.getY()<= 550) {
 				sprite.translateY(10);
@@ -47,6 +57,7 @@ public class GameScreen implements Screen {
 		*/
 		game.batch.begin();
 		sprite.draw(game.batch);
+		tower_sprite.draw(game.batch);
 		/*
 		game.batch.draw(game.harry
 				, camera.viewportWidth / 2 - harry_width / 2
